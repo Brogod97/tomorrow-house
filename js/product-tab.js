c649d204d3ab82f500d3f21df9005d1f476457fa
Hyeongshin <evergreen1286@gmail.com>
2022-02-19 17:15:02 +0900

    🚧 Detect position of tab-panels

--- js/product-tab.js
+++ js/product-tab.js
@@ -16,12 +16,12 @@ function toggleActivetab() {
   }
 }
 
-function scrollToTabPannel() {
-  const tabPannelId = this.parentNode.getAttribute('aria-labelledby')
-  const tabPannel = document.querySelector(`#${tabPannelId}`)
+function scrollToTabpanel() {
+  const tabpanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabpanel = document.querySelector(`#${tabpanelId}`)
 
   const scrollAmount =
-    tabPannel.getBoundingClientRect().top -
+    tabpanel.getBoundingClientRect().top -
     (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
 
   window.scrollBy({
@@ -32,5 +32,31 @@ function scrollToTabPannel() {
 
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActivetab)
-  button.addEventListener('click', scrollToTabPannel)
+  button.addEventListener('click', scrollToTabpanel)
 })
+
+const productTabpanelIdList = [
+  'product-spec',
+  'product-review',
+  'product-inquiry',
+  'product-shipment',
+  'product-recommendation',
+]
+
+const productTabPanelList = productTabpanelIdList.map((panelId) => {
+  const tabPanel = document.querySelector(`#${panelId}`)
+  return tabPanel
+})
+
+const productTabPanelPositionMap = {}
+
+function detectTabPanelPosition() {
+  productTabPanelList.forEach((panel) => {
+    const id = panel.getAttribute('id')
+    const position = window.scrollY + panel.getBoundingClientRect().top
+    productTabPanelPositionMap[id] = position
+  })
+}
+
+window.addEventListener('load', detectTabPanelPosition)
+window.addEventListener('resize', detectTabPanelPosition)
